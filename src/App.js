4ddb19e7d803fdab03c8ec2ba1c40b3154a3b7d8
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-01 11:46:03 -0700

    set up search with axios and hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
+import key from "./key";
 
 export default () => {
   const [gifs, setGifs] = useState([]);
-  useEffect(() => {});
+  useEffect(() => {
+    axios
+      .get(`https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=25`)
+      .then(({ data: { data } }) => setGifs(() => data))
+      .catch((err) =>
+        console.error("Problem fetching and parsing data: ", err.message)
+      );
+  }, []);
   return (
     <div>
       <div className="main-header">
